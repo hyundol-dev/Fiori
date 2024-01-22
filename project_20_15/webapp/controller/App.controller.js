8977354615991fdbce20cf3278a4cc750008ef3c
hyundol-dev <lhyuns912@gmail.com>
2024-01-22 16:56:48 +0900

    240122 15:00

---
+++ project_20_15/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("project2015.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ project_20_15/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["project2015/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
